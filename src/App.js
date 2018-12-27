1b2908e3e1961ab6356dd6a55d6b01be9390202d
越久村淳 <junokumura@mbp.local>
2018-12-27 18:30:27 +0700

    test API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React, { Component } from 'react'
+import React, { Component,Fragment } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Input from './components/input.js'
 import Button from './components/button.js'
 import TaskList from './components/task.js'
+import axios from 'axios';
+import logo from './logo.svg';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: []
+    };
+    this.getData = this.getData.bind(this);
+  }
+
+  getData() {
+    axios
+      .get('http://localhost:8000/api/test')
+      .then(results => {
+        const data = results.data;
+        console.log(data);
+        this.setState({
+          tasks: [...data]
+        });
+      });
+  }
+
 
   handleLogin = () => {
     var email = document.getElementById("emaill").value;
     var pass = document.getElementById("passs").value;
     
     if(email === "a@a.com" && pass === "a") {
-      alert(" something")
+      alert("correct")
     } else {
       alert("wrong something")
     }
 
   }
   render() {
+    const tasks = this.state.tasks.map(task => {
+      return <li key={task.id}>{task.title}</li>;
+    });
     return (
       <div className="wrapper">
+      <Fragment>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+
+        <div>
+          <button onClick={this.getData}>getData</button>
+          <ul>
+            {tasks}
+          </ul>
+        </div>
+      </Fragment>
         <p>Diary</p>
         <p>Welcome back!</p>
         <form id="login-form" name="mainForm">
